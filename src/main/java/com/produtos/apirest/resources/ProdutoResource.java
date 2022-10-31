e55ee41e331d981029e19eeaea4aec7846d53f39
willianlim <willianrosendodelima@outlook.com>
2022-10-31 09:42:16 -0300

    route mapping for request

---
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -0,0 +1,32 @@
+package com.produtos.apirest.resources;
+
+import com.produtos.apirest.models.Produto;
+import com.produtos.apirest.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ProdutoResource {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @GetMapping("/produtos")
+    public List<Produto> listaProdutos() {
+        return (produtoRepository.findAll());
+    }
+
+    @GetMapping("/produtos/{id}")
+    public Produto listaProdutosUnico(@PathVariable(value = "id") long id) {
+        return (produtoRepository.findById(id));
+    }
+
+    @PostMapping("/produto")
+    public Produto savaProduto(@RequestBody Produto produto) {
+        return (produtoRepository.save(produto));
+    }
+
+}
