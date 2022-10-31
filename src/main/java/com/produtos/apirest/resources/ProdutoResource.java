71c3b9b7f96cbf1d3da30cc2830cd270a152e991
willianlim <willianrosendodelima@outlook.com>
2022-10-31 18:28:12 -0300

    signaling for the swagger

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -2,6 +2,7 @@ package com.produtos.apirest.resources;
 
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +15,31 @@ public class ProdutoResource {
     @Autowired
     ProdutoRepository produtoRepository;
 
+    @ApiOperation(value="Retorna uma lista de Produtos")
     @GetMapping("/produtos")
     public List<Produto> listaProdutos() {
         return (produtoRepository.findAll());
     }
 
+    @ApiOperation(value="Retorna um produto unico")
     @GetMapping("/produtos/{id}")
     public Produto listaProdutosUnico(@PathVariable(value = "id") long id) {
         return (produtoRepository.findById(id));
     }
 
+    @ApiOperation(value="Salvar um produto")
     @PostMapping("/produto")
     public Produto savaProduto(@RequestBody Produto produto) {
         return (produtoRepository.save(produto));
     }
 
+    @ApiOperation(value="Deleta um produto")
     @DeleteMapping("/produto")
     public void deltaProduto(@RequestBody Produto produto) {
         produtoRepository.delete(produto);
     }
 
+    @ApiOperation(value="Atualiza um produto")
     @PutMapping("/produto")
     public Produto atalizaProduto(@RequestBody Produto produto) {
         return (produtoRepository.save(produto));
